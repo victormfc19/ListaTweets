194b051acee4de10ba8bc32b7a19ffeafca74095
VictorFlorez <victor_florez82171@elpoli.edu.co>
2020-09-09 15:23:38 -0500

    Se leen los tweets y se muestra mensaje de error

--- js/app.js
+++ js/app.js
@@ -3,20 +3,37 @@ const formulario = document.querySelector('#formulario');
 const listaTweets =  document.querySelector('#lista-tweets');
 
 
-
-
 // event listeners 
 
 eventListeners();
 
 function eventListeners(){
-    formulario.addEventListener('submit', agregarTweet )
+   formulario.addEventListener('submit', agregarTweet )
 }
-
 
 // funciones
 
-
-function agregarTweet(){
+function agregarTweet(e){
+    e.preventDefault();
     
+    const tweet = document.querySelector('#tweet').value;
+    if(tweet == ''){
+        mostrarError('No puede ir vacio');
+        return; 
+    }
+    else
+        console.log(tweet);
+}
+
+
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error'); // le agrego la clase error: declara en la hoja de estilos custom.css
+
+    document.querySelector('#contenido').appendChild(mensajeError);
+
+    setTimeout( () => {
+        mensajeError.remove(); // Eliminar el letrero de error despues de 3 segundos
+    }, 3000);
 }
