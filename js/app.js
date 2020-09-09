e127767f3dc3f1ca42d0f2e5fa6da5e3098463db
VictorFlorez <victor_florez82171@elpoli.edu.co>
2020-09-09 13:29:24 -0500

    Empezando proyecto tweets

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// variables 
+const formulario = document.querySelector('#formulario');
+const listaTweets =  document.querySelector('#lista-tweets');
+
+
+
+
+// event listeners 
+
+eventListeners();
+
+function eventListeners(){
+    formulario.addEventListener('submit', agregarTweet )
+}
+
+
+// funciones
+
+
+function agregarTweet(){
+    
+}
